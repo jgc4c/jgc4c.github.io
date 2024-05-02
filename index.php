aabe8088f9fe44195dbc4a66dcc3ca27c85f172a
geeb <jgc4c@mtmail.mtsu.edu>
2024-05-02 16:26:06 -0500

    Changed index.php more.

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Welcome to my Website! Below you will find my projects.</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis dolorem sequi atque, est dolores odit, doloribus quae ad explicabo consequatur alias libero iusto esse sed. Perferendis numquam iusto sapiente sint?</p>
 </body>
 </html>
